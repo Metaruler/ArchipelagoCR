798d9d35fb9c279894e529ed1315f88d3f39a26b
Metaruler <120737513+Metaruler@users.noreply.github.com>
2025-09-08 22:03:45 -0500

    Update locations.py

--- worlds/custom_robo/locations.py
+++ worlds/custom_robo/locations.py
@@ -1,1 +1,18 @@
 #Checks
+from typing import NamedTuple, Dict, Optional
+from .Items import CRItemData
+from .Helpers import CRRawData
+
+class CRLocationData(NamedTuple):
+  name:str
+  code:Optional[int]
+  ram_addr: Optional[CRRawData] = None
+
+# Begin check logic
+BATTLE_COUNTER: dict[str, CRLocationData] = {
+  "Battles Won": CRLocationData(
+    name="Battle 1",
+    code=1,
+    ram_addr=CRRawData(0x803BE7A7, ram_byte_size=7)
+  )
+}
