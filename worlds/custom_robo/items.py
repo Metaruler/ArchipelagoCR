0625730a628b578c9897996647d043b8622c8d30
Metaruler <120737513+Metaruler@users.noreply.github.com>
2025-09-08 21:33:42 -0500

    Update items.py

--- worlds/custom_robo/items.py
+++ worlds/custom_robo/items.py
@@ -1,1 +1,13 @@
+from typing import NamedTuple, Dict, Optional, Set
 
+from BaseClasses import Item
+from BaseClasses import ItemClassification as IC
+
+# Item Class
+class CRItemData(NamedTuple):
+  type: str
+  code: Optional[int]
+  classification: IC
+  update_ram_addr: Optional[list[CRRawData]] = None
+
+# Begin item list for AP
