2d3839fcf6fb8446e221a5fa9a09b0c54246fbb2
Metaruler <120737513+Metaruler@users.noreply.github.com>
2025-09-08 21:47:32 -0500

    Update items.py

--- worlds/custom_robo/items.py
+++ worlds/custom_robo/items.py
@@ -11,3 +11,25 @@ class CRItemData(NamedTuple):
   update_ram_addr: Optional[list[CRRawData]] = None
 
 # Begin item list for AP
+PROGRESSION_ITEM_TABLE: dict[str, CRItemData] = {
+  "Progressive Scenario": CRItemData(
+    type="Key Item",
+    code=1,
+    classification=IC.progression,
+    update_ram_addr=None
+  )
+}
+
+PARTS_ITEM_TABLE: dict[str, CRItemData] = {
+  "Ray 01": CRItemData(
+    type="Body",
+    code=2,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9F, bit_position=1)]
+  )
+}
+
+ALL_ITEMS_TABLE = {
+  **PROGRESSION_ITEM_TABLE,
+  **PARTS_ITEM_TABLE
+}
