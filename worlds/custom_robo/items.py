a44c19cc4e27f6861b95570185f20da64e7b04dd
Metaruler <120737513+Metaruler@users.noreply.github.com>
2025-09-11 19:26:08 -0500

    Update items.py

--- worlds/custom_robo/items.py
+++ worlds/custom_robo/items.py
@@ -132,7 +132,7 @@ PROGRESSION_SCENARIO_TABLE: dict[str, CRItemData] = {
       classification=IC.progression,
       item_id=17,
       update_ram_addr=None
-    ),
+    )
   ]
 }
 
@@ -142,10 +142,223 @@ PARTS_ITEM_TABLE: dict[str, CRItemData] = {
     code=18,
     classification=IC.useful,
     update_ram_addr=[CRRawData(0x803BFB9F, bit_position=1)]
-  )
+  ),
+  "Splendor": CRItemData(
+    type="Body",
+    code=19,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9F, bit_position=2)]
+  ),
+  "Glory": CRItemData(
+    type="Body",
+    code=20,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9F, bit_position=3)]
+  ),
+  "Milky Way": CRItemData(
+    type="Body",
+    code=21,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9F, bit_position=4)]
+  ),
+  "Earth": CRItemData(
+    type="Body",
+    code=22,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9F, bit_position=5)]
+  ),
+  "Sol": CRItemData(
+    type="Body",
+    code=23,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9F, bit_position=6)]
+  ),
+  "Metal Ape": CRItemData(
+    type="Body",
+    code=24,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9F, bit_position=7)]
+  ),
+  "Metal Bear": CRItemData(
+    type="Body",
+    code=25,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9F, bit_position=8)]
+  ),
+  "Metal Ox": CRItemData(
+    type="Body",
+    code=26,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9E, bit_position=1)]
+  ),
+  "Swift": CRItemData(
+    type="Body",
+    code=27,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9E, bit_position=2)]
+  ),
+  "Shrike": CRItemData(
+    type="Body",
+    code=28,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9E, bit_position=3)]
+  ),
+  "Peregrine": CRItemData(
+    type="Body",
+    code=29,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9E, bit_position=4)]
+  ),
+  "Javelin": CRItemData(
+    type="Body",
+    code=30,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9E, bit_position=5)]
+  ),
+  "Glaive": CRItemData(
+    type="Body",
+    code=31,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9E, bit_position=6)]
+  ),
+  "Halberd": CRItemData(
+    type="Body",
+    code=32,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9E, bit_position=7)]
+  ),
+  "Criminal": CRItemData(
+    type="Body",
+    code=33,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9E, bit_position=8)]
+  ),
+  "Buggy": CRItemData(
+    type="Body",
+    code=34,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9D, bit_position=1)]
+  ),
+  "Juggler": CRItemData(
+    type="Body",
+    code=35,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9D, bit_position=2)]
+  ),
+  "Defender": CRItemData(
+    type="Body",
+    code=36,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9D, bit_position=3)]
+  ),
+  "Seeker": CRItemData(
+    type="Body",
+    code=37,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9D, bit_position=4)]
+  ),
+  "Breaker": CRItemData(
+    type="Body",
+    code=38,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9D, bit_position=5)]
+  ),
+  "Seal Head": CRItemData(
+    type="Body",
+    code=39,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9D, bit_position=6)]
+  ),
+  "Dour Head": CRItemData(
+    type="Body",
+    code=40,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9D, bit_position=7)]
+  ),
+  "Tank Head": CRItemData(
+    type="Body",
+    code=41,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9D, bit_position=8)]
+  ),
+  "Ray Legend": CRItemData(
+    type="Body",
+    code=42,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9C, bit_position=1)]
+  ),
+  "Oil Can": CRItemData(
+    type="Body",
+    code=43,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9C, bit_position=2)]
+  ),
+  "Ray Warrior": CRItemData(
+    type="Body",
+    code=44,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9C, bit_position=3)]
+  ),
+  "Rakansen": CRItemData(
+    type="Body",
+    code=45,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9C, bit_position=4)]
+  ),
+  "Ruhiel": CRItemData(
+    type="Body",
+    code=46,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9C, bit_position=5)]
+  ),
+  "Athena": CRItemData(
+    type="Body",
+    code=47,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9C, bit_position=6)]
+  ),
+  "Rahu I": CRItemData(
+    type="Body",
+    code=48,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9C, bit_position=7)]
+  ),
+  "Rahu II": CRItemData(
+    type="Body",
+    code=44,
+    classification=IC.useful,
+    update_ram_addr=[CRRawData(0x803BFB9C, bit_position=3)]
+  ),
+}
+
+PROGRESSION_RAHU_BODY: dict[str, CRItemData] = {
+  "Rahu Body Upgrade": [
+    CRItemData(
+      type="Rahu I",
+      code=1,
+      classification=IC.progression,
+      item_id=1,
+      update_ram_addr=None
+    ),
+    CRItemData(
+      type="Rahu II",
+      code=2,
+      classification=IC.progression,
+      item_id=2,
+      update_ram_addr=None
+    ),
+    CRItemData(
+      type="Rahu III",
+      code=3,
+      classification=IC.progression,
+      item_id=3,
+      update_ram_addr=None
+    )
+  ]
 }
 
 ALL_ITEMS_TABLE = {
   **PROGRESSION_SCENARIO_TABLE,
   **PARTS_ITEM_TABLE
+  **PROGRESSION_RAHU_BODY
 }
