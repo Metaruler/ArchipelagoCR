720b2c46e3159710fc6b18561570c192f315f7c7
Metaruler <120737513+Metaruler@users.noreply.github.com>
2025-09-18 21:53:41 -0500

    Update locations.py

--- worlds/custom_robo/locations.py
+++ worlds/custom_robo/locations.py
@@ -9,20 +9,20 @@ class CRLocationData(NamedTuple):
   ram_addr: Optional[CRRawData] = None
 
 # Begin check logic
-# Each battle counts as a check
-BATTLE_COUNTER: dict[str, CRLocationData] = {
-  "Battles Won": CRLocationData(
-    name="Battles",
-    code=1,
-    ram_addr=CRRawData(0x803BE7A7, ram_byte_size=7)
-  )
-}
-
 # Check for each part being used in a victory
 PART_USE: dict[str, CRLocationData] = {
   "Use Ray 01": CRLocationData(
     name="Battles",
-    code=2,
+    code=1,
+    ram_addr=CRRawData(0x803BFBA7, bit=1)
+  )
+}
+
+# Each battle counts as a check
+BATTLE_COUNTER: dict[str, CRLocationData] = {
+  "Battles Won": CRLocationData(
+    name="Battles",
+    code=???,
     ram_addr=CRRawData(0x803BE7A7, ram_byte_size=7)
   )
 }
@@ -30,6 +30,6 @@ PART_USE: dict[str, CRLocationData] = {
 
 
 LOCATION_TABLE: dict[str, CRLocationData] = {
-  **BATTLE_COUNTER,
-  **PART_USE
+  **PART_USE,
+  **BATTLE_COUNTER
 }
